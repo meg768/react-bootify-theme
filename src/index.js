c47cc3c438feec670d15c4fa652a4c3ada2cff5b
meg768 <meg768@users.noreply.github.com>
2018-10-06 23:47:33 +0200

    -

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
-import Theme from './theme.js';
-export default Theme;
+
+export default class Theme extends Component {
+
+    constructor(props) {
+        super(props);
+
+        if (this.props.name) {
+            require('bootswatch/dist/' + this.props.name + '/bootstrap.min.css');
+        }
+        else {
+            require('bootstrap-css-only/css/bootstrap.min.css');
+        }
+
+    }
+
+    render() {
+        return this.props.children;
+    }
+}
