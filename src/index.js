e0742206b534c4fb165abe206e6d3ee5ccb0cbfb
meg768 <meg768@users.noreply.github.com>
2018-10-07 00:08:58 +0200

    -

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import React from 'react';
 
 
+export default class extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        if (this.props.name) {
+            require('bootswatch/dist/' + this.props.name + '/bootstrap.min.css');
+        }
+        else {
+            require('bootstrap-css-only/css/bootstrap.min.css');
+        }
+
+    }
+
+    render() {
+        return this.props.children;
+    }
+}
+
+/*
 export class Bootstrap extends React.Component {
 
     constructor(props) {
@@ -31,3 +51,5 @@ export class Bootswatch extends React.Component {
         return this.props.children;
     }
 }
+1
+*/
